d609a96c6a93e74006f5e88ecc6f8063058d990d
aleksei2011156 <alekseipushkarskii97@gmail.com>
2021-10-14 15:54:22 +0300

    Start and Finish

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+"""Определяет схемы URL для пользователя."""
+from django.urls import path, include
+from .import views
+
+app_name = 'users'
+urlpatterns = [
+    #Включить URL авторизации по умолчанию.
+    path('', include('django.contrib.auth.urls')),
+    #Страница для регистрации.
+    path('register/', views.register, name='register'),
+]
